ce5e9ba2bf0732c38515f22a2402ce8e23fe4fed
Aditya Singh <adityasingh740745@gmail.com>
2026-06-27 19:59:51 +0530

    feat: implement chat loop for LLM interaction and add new profile

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log("Hello via Bun!");
+import { createProvider } from "./src/providers/index";
+import { runLoop } from "./src/agent/loop";
+
+const llm = createProvider("openrouter-nemotron-free");
+await runLoop(llm);
---
+++ src/agent/loop.ts
@@ -0,0 +1,36 @@
+// 0. lets create fn instead class cuz i didnt need to manage state on any object, only history which can be created as a simple array of messages(for now)
+// 1. store the history of messages
+// 2. create a loop that runs until the user exits(while true)
+// 3. prompt the user for input
+// 4. send the input + history to the llm and get the response
+// 5. now save the input and reply to the history
+// 6. print the reply
+// 7. repeat
+import type { LLMProvider, Message } from "../providers/types";
+
+export async function runLoop(llm: LLMProvider) {
+    const history: Message[] = [];
+
+    while (true) {
+        const userInput = prompt(">>");
+        if (userInput === null || !userInput.trim()) continue;
+        const tempHistory: Message[] = [
+            ...history,
+            { role: "user", content: userInput },
+        ];
+        console.log("AI is thinking...")
+        const reply = await llm.chat(tempHistory);
+        history.push(
+            {
+                role: "user",
+                content: userInput,
+            },
+            {
+                role: "assistant",
+                content: reply,
+            },
+        );
+
+        console.log(`AI: ${reply}`);
+    }
+}
