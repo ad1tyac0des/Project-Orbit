005ff157f87c650e7eb959497c6b1511581210f7
Aditya Singh <adityasingh740745@gmail.com>
2026-07-23 01:48:06 +0530

    add sessions table and accordingly updated memories table

--- src/memory/db.ts
+++ src/memory/db.ts
@@ -6,20 +6,35 @@ mkdirSync("data", { recursive: true }); // create data folder if it doesn't exis
 
 export function connectDB() {
     const db = new Database("data/orbit.db");
-    db.run("PRAGMA journal_mode=WAL");
+    db.run("PRAGMA journal_mode=WAL;");
+    db.run("PRAGMA foreign_keys=ON;")
     return db;
 }
 
 export function initSchema(db: Database) {
-    // Create Table
+    // Sessions Table
     db.run(`
-    CREATE TABLE IF NOT EXISTS memories (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        role TEXT NOT NULL,
-        content TEXT NOT NULL,
-        created_at INTEGER DEFAULT (unixepoch())
-    )
-`);
+        CREATE TABLE IF NOT EXISTS sessions (
+            id          TEXT PRIMARY KEY DEFAULT (lower(hex(randomblob(16)))),
+            created_at  INTEGER DEFAULT (unixepoch()),
+            updated_at  INTEGER DEFAULT(unixepoch()),
+            ended_at    INTEGER,
+            title       TEXT NOT NULL
+        )
+    `);
+
+    // Memories Table
+    db.run(`
+        CREATE TABLE IF NOT EXISTS memories (
+            id          INTEGER PRIMARY KEY,
+            session_id  TEXT NOT NULL,
+            created_at  INTEGER DEFAULT (unixepoch()),
+            model       TEXT,
+            role        TEXT NOT NULL,
+            content     TEXT NOT NULL,
+            FOREIGN KEY (session_id) REFERENCES sessions(id)
+        )
+    `);
 }
 
 export function loadHistory(db: Database): Message[] {
@@ -29,6 +44,8 @@ export function loadHistory(db: Database): Message[] {
 }
 
 export function saveMessage(db: Database, message: Message) {
-    const insert = db.query(`INSERT INTO memories (role, content) VALUES ($role, $content)`);
-    insert.run({ $role: message.role, $content: message.content })
+    const insert = db.query(
+        `INSERT INTO memories (role, content) VALUES ($role, $content)`,
+    );
+    insert.run({ $role: message.role, $content: message.content });
 }
